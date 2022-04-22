a2ef2d35ac0616eece028a743fc96548c808438b
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-04-21 17:37:30 -0700

    Added delete for temp in printLineage and computeRelation in Person

--- person.cpp
+++ person.cpp
@@ -6,7 +6,7 @@ using std::cout;
 using std::endl;
 
 Person::Person(const char *name_, Person* father_, Person* mother_){
-    name = new char[strlen(name_)];
+    name = new char[strlen(name_)+1];
     strcpy(name, name_);
     father = father_;
     mother = mother_;
@@ -16,7 +16,7 @@ Person::Person(const char *name_, Person* father_, Person* mother_){
 }
 
 Person::~Person(){
-    delete children;
+    delete[] children;
 }
 
 void Person::addChild(Person *newChild){
@@ -52,6 +52,8 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
+
+	delete temp;
 }
 
 /* helper function to compute the lineage
@@ -66,7 +68,8 @@ char* Person::compute_relation(int level){
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
-        temp = temp2;
+        delete [] temp;
+	temp = temp2;
     }
     return temp;
 }
